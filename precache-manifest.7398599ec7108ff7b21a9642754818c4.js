3e31e04f2638efc08efe7d307a55adc384132101
leon <li.zhiang@cgtn.com>
2020-02-23 22:11:48 +0800

    Updates

---
+++ precache-manifest.7398599ec7108ff7b21a9642754818c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b3cf82d669a0b111ac1785ed8bbff15",
+    "url": "/lza/lza.github.io/index.html"
+  },
+  {
+    "revision": "1732644e0f5b6528ecc3",
+    "url": "/lza/lza.github.io/static/css/main.77fa4355.chunk.css"
+  },
+  {
+    "revision": "77ed8ed3b85b21ad1d8d",
+    "url": "/lza/lza.github.io/static/js/2.80f6a57b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/lza/lza.github.io/static/js/2.80f6a57b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1732644e0f5b6528ecc3",
+    "url": "/lza/lza.github.io/static/js/main.b57415e7.chunk.js"
+  },
+  {
+    "revision": "7768a45f6c7977aa84fa",
+    "url": "/lza/lza.github.io/static/js/runtime-main.5d22784a.js"
+  }
+]);
